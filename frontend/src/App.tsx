4a0335c6ffcea29a420c7de4ad240a43500de2b4
emmahelquist <154380901+emmahelquist@users.noreply.github.com>
2025-04-08 09:51:42 -0600

    Added admin panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,26 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/HomePage";
 import MovieListPage from "./pages/MovieListPage";
 import MovieInfoPage from "./pages/MovieInfoPage";
 import LoginPage from "./pages/LoginPage";
-import PrivacyPage from "./pages/PrivacyPage";
 import UserDashboardPage from "./pages/UserDashboardPage";
-import Footer from "./components/Footer";
-import "./App.css";
-import CreateAccountStep1 from "./components/CreateAccountStep1";
-import CreateAccountStep2 from "./components/CreateAccountStep2";
+import PrivacyPage from "./pages/PrivacyPage";
+import AdminMoviesPage from "./pages/AdminMoviesPage";
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MovieListPage />} />
         <Route path="/movie/:id" element={<MovieInfoPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
-        <Route path="/signup" element={<CreateAccountStep1 />} />
-        <Route path="/signup/step2" element={<CreateAccountStep2 />} />
         <Route path="/dashboard" element={<UserDashboardPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/admin/movies" element={<AdminMoviesPage />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
